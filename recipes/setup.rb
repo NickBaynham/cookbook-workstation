bbbb4438e21477c5b7ce3f28fce3e65f27374c79
Nick Baynham <nickbaynham@gmail.com>
2021-01-02 19:35:29 +0000

    added template

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,14 +5,7 @@ end
 package 'ntp'
 package 'git'
 
-file '/etc/motd' do
-	content "This server is the property of Nick Baynham
-	HOSTNAME: #{node['hostname']}
-	IPADDRESS: #{node['ipaddress']}
-	Memory: #{node['memory']['total']}
-	CPU: #{node['cpu']['0']['mhz']}
-	"
-        action :create
-	owner 'root'
-	group 'root'
+template '/etc/motd' do
+	source 'motd.erb'
+	action :create
 end
