2a2dcdc56382252c7908bdac85f8374e08fcc838
vagrant <vagrant@vagrant.vm>
2021-01-02 00:54:48 +0000

    Added recipes/setup.rb to workstation cookbook

---
+++ recipes/setup.rb
@@ -0,0 +1,12 @@
+package 'tree' do
+	action :install
+end
+
+package 'git'
+
+file '/etc/motd' do
+	content 'This server is the property of Scott Steele'
+        action :create
+	owner 'root'
+	group 'root'
+end
